84c8579eee8641e5140cc9f1e8e02624f251c106
RP <31228656+renpengpeng@users.noreply.github.com>
2018-01-08 12:37:20 +0800

    优化路由

--- application/route.php
+++ application/route.php
@@ -25,57 +25,65 @@ use think\Route;
 	静态路由
 */
 
-// 首页路由
-Route::rule('/','index/index/index');
-
-// 网址提交
-Route::rule('/addurl','index/addurl/index');
-
-// 网址提交页面
-Route::rule('/addurls','index/addurl/adds');
-
-// 404页面
-Route::rule('/404','index/index/errors');
-
-// sitemap begin
-
-Route::any('/sitemap','index/sitemap/index','[]',['ext'=>'xml','denyext'=>'']);
-Route::any('/sitemap_list','index/sitemap/lists','[]',['ext'=>'xml','denyext'=>'']);
-Route::any('/sitemap_siteinfo','index/sitemap/siteinfo','[]',['ext'=>'xml','denyext'=>'']);
-Route::any('/sitemap_search','index/sitemap/search','[]',['ext'=>'xml','denyext'=>'']);
-Route::any('/sitemap_seo','index/sitemap/seo','[]',['ext'=>'xml']);
-Route::any('/sitemap_article','index/sitemap/article','[]',['ext'=>'xml']);
 
 
-// sitemap end
+
+Route::rule([
+	// 首页路由
+	'/'					=>	'index/index/index',
+
+	// 网址提交
+	'/addurl'			=>	'index/addurl/index',
+
+	// 网址提交处理页面
+	'/addurls'			=>	'index/addurl/adds',
+
+	// 404页面
+	'/404'				=>	'index/index/errors',
+
+	// 网站搜索
+	'/search'			=>	'index/search/views',
+
+	// 搜索处理页面
+	'/searchs'			=>	'index/search/index',
+
+	// SEO综合查询页面
+	'/seo'				=>	'index/seo/index',
+
+	// 文章列表
+	'article'			=>	'index/addarticle/lists',
+
+	// 文章投稿
+	'/addarticle'		=>	'index/addarticle/index',
+
+	// 文章投稿处理
+	'/addarticles'		=>	'index/addarticle/adds',
+
+	// 文章评论
+	'/article_common'	=>	'index/addarticle/common',
+
+	// 网站地图
+	'/sitemap'			=>	'index/sitemap/index',
+	'/sitemap_list'		=>	'index/sitemap/lists',
+	'/sitemap_siteinfo'	=>	'index/sitemap/siteinfo',
+	'/sitemap_search'	=>	'index/sitemap/search',
+	'/sitemap_seo'		=>	'index/sitemap/seo',
+	'/sitemap_article'	=>	'index/sitemap/article',
+
+]);
 
 
-// search
-Route::get('/searchs','index/search/index');
-Route::rule('/search','index/search/views');
-
-// seo
-Route::any('/seo','index/seo/index'); 
-
-// 文章路由
-Route::rule('/addarticle','index/addarticle/index');
-Route::any('/addarticles','index/addarticle/adds');
-
-// 文章列表
-Route::rule('article','index/addarticle/lists');
-
-Route::get('arcicle/:id','index/addarticle/look',['ext'=>'html']);
-
-// 评论路由
-Route::rule('/article_common','index/addarticle/common');
 
 // 批量注册get
 Route::rule([
 	// url路由
 	'siteinfo/:id'			=>	'index/siteinfo/index',
+
 	// 分类路由
 	'list/:id'				=>	'index/cate/index',
+
 	// 文章
+	'/article_a/:id'		=>	'index/addarticle/look',
+
 ],'','get',['ext'=>'html']);
 
-// 后台
